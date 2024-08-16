d21cab0f956a01a20ece35ea5ced67ca3cc0dd24
SURBHI JAIN <121885461+Surbhi399@users.noreply.github.com>
2024-08-16 17:18:36 +0530

    Keeper App(Part-1)
    It contains Header ,Footer, Note component  for Keeper App

---
+++ keeper-app-part-1/src/Components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Footer />
+    </>
+  );
+}
+export default App;
---
+++ keeper-app-part-1/src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright @ {year}</p>
+    </footer>
+  );
+}
+export default Footer;
