a16fc420b13689662b022932973f8fc15813ff8a
SURBHI JAIN <121885461+Surbhi399@users.noreply.github.com>
2025-07-07 00:09:25 +0530

    Joke list for practice of props de-structure

---
+++ Joke list using props destructure/App.jsx
@@ -0,0 +1,46 @@
+/**
+ * Challenge: create a page that displays your favorite jokes
+ * - Create a Joke component in its own file.
+ * - Import and render 4-5 <Joke /> components
+ * - Each Joke should receive a "setup" prop and a "punchline" prop
+ *   and render those however you'd like
+ * - Use your favorite 2-part jokes (setup & punchline), or check
+ *   jokes.md file for some examples.
+ * 
+ * EXTRA CREDIT:
+ * Some jokes are only a punchline with no setup:
+ * 
+ * E.g.: "It’s hard to explain puns to kleptomaniacs because 
+ * they always take things literally."
+ * 
+ * If you don't pass in a "question" prop, how might you make it only 
+ * show the punchline?
+ */
+import Joke from "/Components/Joke"
+export default function App() {
+    return (
+        <>
+        <Joke 
+            setup = "I got my daughter a fridge for her birthday."
+            punchline = "I can't wait to see her face light up when she opens it."
+            
+        />
+         <Joke 
+            setup = "How did the hacker escape the police?"
+            punchline = " He just ransomware!"
+         />
+          <Joke 
+          setup= "Why don't pirates travel on mountain roads?"
+          punchline= "Scurvy."
+          />
+           <Joke
+          setup= "Why do bees stay in the hive in the winter?"
+          punchline= "Swarm."
+            />
+            <Joke
+             setup= "Why do bees stay in the hive in the winter?"
+             punchline= "Swarm."
+             />
+        </>
+    )
+}
---
+++ Joke list using props destructure/Components/Joke.jsx
@@ -0,0 +1,7 @@
+export default function Joke({setup, punchline}){
+    return(
+    <main>
+    <h3>{setup}</h3>
+    <p>{punchline}</p>
+    </main>)
+}
